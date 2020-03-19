067af20e1e3378e729eadafa3cf63c195e2ac86d
Peter Hall <33176108+IamPete1@users.noreply.github.com>
2020-03-19 16:18:25 +0000

    AP_Logger: LogStructure: add AIS

--- libraries/AP_Logger/LogStructure.h
+++ libraries/AP_Logger/LogStructure.h
@@ -133,6 +133,7 @@ const struct MultiplierStructure log_Multipliers[] = {
 #include <AC_PrecLand/LogStructure.h>
 #include <AC_Avoidance/LogStructure.h>
 #include <AP_ESC_Telem/LogStructure.h>
+#include <AP_AIS/LogStructure.h>
 
 // structure used to define logging format
 struct LogStructure {
@@ -1336,7 +1337,12 @@ LOG_STRUCTURE_FROM_VISUALODOM \
       { LOG_PSCE_MSG, sizeof(log_PSCE), \
         "PSCE", "Qffffffff", "TimeUS,TPE,PE,DVE,TVE,VE,DAE,TAE,AE", "smmnnnooo", "F00000000" }, \
       { LOG_PSCD_MSG, sizeof(log_PSCD), \
-        "PSCD", "Qffffffff", "TimeUS,TPD,PD,DVD,TVD,VD,DAD,TAD,AD", "smmnnnooo", "F00000000" }
+        "PSCD", "Qffffffff", "TimeUS,TPD,PD,DVD,TVD,VD,DAD,TAD,AD", "smmnnnooo", "F00000000" }, \
+    { LOG_PSC_MSG, sizeof(log_PSC), \
+      "PSC", "Qffffffffffff", "TimeUS,TPX,TPY,PX,PY,TVX,TVY,VX,VY,TAX,TAY,AX,AY", "smmmmnnnnoooo", "F000000000000", true }, \
+    { LOG_PSCZ_MSG, sizeof(log_PSCZ), \
+      "PSCZ", "Qfffffffff", "TimeUS,TPZ,PZ,DVZ,TVZ,VZ,DAZ,TAZ,AZ,ThO", "smmnnnooo%", "F000000002", true }, \
+LOG_STRUCTURE_FROM_AIS \
 
 // @LoggerMessage: SBPH
 // @Description: Swift Health Data
@@ -1451,6 +1457,8 @@ enum LogMessages : uint8_t {
     LOG_PSCD_MSG,
     LOG_RAW_PROXIMITY_MSG,
     LOG_IDS_FROM_PRECLAND,
+    LOG_IDS_FROM_AIS,
+
 
     _LOG_LAST_MSG_
 };
